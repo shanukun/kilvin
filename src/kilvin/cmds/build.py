336fbc6a6abe4509f8f9055824df8093716fa4df
Shanu <prathms007@gmail.com>
2023-02-20 11:33:29 +0530

    build: Fix relative path href.

--- src/kilvin/cmds/build.py
+++ src/kilvin/cmds/build.py
@@ -1,24 +1,87 @@
 import os
 import pathlib
 
+import frontmatter
+
 from kilvin.mdown import markdown
+
+DIR_CONTENT = "content"
+DIR_PUBLIC = "public"
+
+
+class CFile:
+    def __init__(self, name, rel_path):
+        self.name = name
+        self.rel_path = rel_path
+
+        self.stem = pathlib.Path(name).stem
+        self.ext = pathlib.PurePath(name).suffix
+
+        self.md_path = pathlib.PurePath(DIR_CONTENT) / self.rel_path / self.name
+        self.dir_path = pathlib.Path(DIR_PUBLIC) / self.rel_path / self.stem
+
+        self.fmatter = self._get_fmatter()
+        self.temp = "list.html" if self.stem == "_index" else "default.html"
+
+        self._make_dir()
+        self._copy_file()
+
+    def _get_fmatter(self):
+        return frontmatter.load(self.md_path)
+
+    def _make_dir(self):
+        if self.stem != "_index" and self.ext == ".md":
+            self.dir_path.mkdir()
+
+    def _copy_file(self):
+        if self.ext != ".md":
+            pass
+
+    def _gen_rel_path(self, p):
+        return self.rel_path / p
+
+    def get_file_list(self):
+        if self.stem == "_index":
+            p = pathlib.Path(DIR_CONTENT) / self.rel_path
+            return [
+                self._gen_rel_path(f.stem) if f.is_dir() else self._gen_rel_path(f.stem)
+                for f in p.iterdir()
+                if f.stem != self.stem
+            ]
+        else:
+            return []
+
+    def get_html_path(self):
+        if self.stem == "_index":
+            return (
+                pathlib.PurePath(DIR_PUBLIC)
+                / self.rel_path
+                / pathlib.PurePath("index.html")
+            )
+        else:
+            return self.dir_path / pathlib.PurePath("index.html")
 
 
 def build_proj():
-    for root, dirs, files in os.walk("./content"):
-        root_path_parts = list(pathlib.PurePath(root).parts)
-        del root_path_parts[0]
-        public_path = pathlib.Path("./public") / "/".join(root_path_parts)
+    md_files = []
+    for root, dirs, files in os.walk(DIR_CONTENT):
+        rel_cur_path = "/".join(list(pathlib.PurePath(root).parts)[1:])
+
+        public_path = pathlib.Path(DIR_PUBLIC) / rel_cur_path
+
         for dir in dirs:
             dir_path = public_path / dir
             dir_path.mkdir()
+
         for file in files:
-            md_file = pathlib.PurePath(root) / file
-            html_file = public_path / file
-            print(html_file)
-            html_file = html_file.with_suffix('.html')
-            out = markdown.render(md_file)
-            with open(html_file, "w") as hf:
-                hf.write(out)
+            rel_path = pathlib.Path(rel_cur_path)
+            md_files.append(CFile(file, rel_path))
+
+    for cfile in md_files:
+        out = markdown.render(
+            cfile.md_path, tmp=cfile.temp, files=cfile.get_file_list()
+        )
+        with open(cfile.get_html_path(), "w") as f:
+            f.write(out)
 
     print("Building finished.")
--- src/kilvin/mdown/markdown.py
+++ src/kilvin/mdown/markdown.py
@@ -1,28 +1,29 @@
-import pathlib
-
 import frontmatter
 import jinja2
 import markdown
 from markdown.extensions.codehilite import CodeHiliteExtension
 
 
-def render(md_path):
+def render(md_path, tmp="./default.html", files=[]):
     tl = jinja2.FileSystemLoader("./layouts")
     env = jinja2.Environment(loader=tl)
     with open(md_path) as f:
         post = frontmatter.load(f)
 
-        temp = env.get_template("./default.html")
+        temp = env.get_template(tmp)
         html = markdown.markdown(
             post.content,
             extensions=[
                 CodeHiliteExtension(),
                 "fenced_code",
-                "sane_lists",
                 "footnotes",
                 "md_in_html",
+                "sane_lists",
+                "tables",
+                "abbr",
+                "legacy_attrs",
             ],
         )
 
-        out = temp.render(title=post["title"], content=html)
+        out = temp.render(title=post["title"], content=html, files=files)
     return out
