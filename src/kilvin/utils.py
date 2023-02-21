82a88bd69ad53ef89a402c594ae12208db19f16b
Shanu <prathms007@gmail.com>
2023-02-21 20:56:49 +0530

    utils: Change is_kilvin_dir to a decorator.

---
+++ src/kilvin/utils.py
@@ -0,0 +1,37 @@
+import functools
+import pathlib
+import shutil
+import sys
+
+DIR_PUBLIC = "public"
+
+
+def is_kilvin_dir(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        cwd = pathlib.Path(".")
+        config_path = cwd / "config.toml"
+        if not config_path.exists():
+            print("Error: Unable to locate config file.")
+            sys.exit(1)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def clean_public():
+    path = pathlib.Path(DIR_PUBLIC)
+
+    if not path.exists():
+        print(f"{DIR_PUBLIC} does not exist")
+        sys.exit(1)
+
+    for fd in path.iterdir():
+        if fd.is_dir():
+            shutil.rmtree(fd)
+        else:
+            fd.unlink()
+
+
+def copy_file(src, dst):
+    shutil.copytree(src, dst)
