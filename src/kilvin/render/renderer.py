7b3976861a2fca38c08250d9cd7de41a0f9a6328
Shanu <prathms007@gmail.com>
2023-03-01 19:53:41 +0530

    renderer: Use dict for storing pages.
    Instead of using a simple list, use dict and store the pages
    to templates.

--- src/kilvin/render/renderer.py
+++ src/kilvin/render/renderer.py
@@ -1,6 +1,8 @@
 import sys
+from collections import defaultdict
 
 import jinja2
+import random
 from markdown import Markdown
 from markdown.extensions.codehilite import CodeHiliteExtension
 from markdown.extensions.toc import TocExtension
@@ -26,32 +28,21 @@ class Renderer:
         self.md_conv = Markdown(extensions=self.EXTENSIONS)
         self.env = jinja2.Environment(loader=self.FS)
 
-        self.pages = []
+        self.pages = defaultdict(list)
         self.tags = []
 
-    def get_default_templt(self, is_index=False):
+    def get_template(self, templt):
         try:
-            if is_index:
-                return "text.html"
-            else:
-                return "text.html"
-        except jinja2.exceptions.TemplateNotFound:
-            print("Error: Default templates not found.")
-            sys.exit(1)
-
-    def get_template(self, page):
-        templt = page.meta.get("template", None)
-        if templt != None:
             return self.env.get_template(templt)
-        else:
-            default_templt = self.get_default_templt(page.is_index)
-            return self.env.get_template(default_templt)
+        except jinja2.exceptions.TemplateNotFound:
+            print(f"Error: Template {templt} not found.")
+            sys.exit(1)
 
     def render_markdown(self, page):
         html = self.md_conv.reset().convert(page.body)
         page.body = html
         self.tags.extend(page.meta.get("tags", ""))
-        self.pages.append(page)
+        self.pages[page.template].append(page)
 
     def build_site(self):
         site = {}
@@ -64,21 +55,21 @@ class Renderer:
     def save_pages(self):
         site = self.build_site()
 
-        for page in self.pages:
-            templ = self.get_template(page)
+        for temp_name in self.pages:
+            templ = self.get_template(temp_name)
+            for page in self.pages[temp_name]:
+                sorted_pages = page.pages
+                sorted_pages.sort()
 
-            sorted_pages = page.pages
-            sorted_pages.sort()
-
-            out = templ.render(
-                cfg=site,
-                meta=page.meta,
-                pages=sorted_pages,
-                dirs=page.dirs,
-                body=page.body,
-            )
-            with open(page.save_path, "w") as f:
-                f.write(out)
+                out = templ.render(
+                    cfg=site,
+                    meta=page.meta,
+                    pages=sorted_pages,
+                    dirs=page.dirs,
+                    body=page.body,
+                )
+                with open(page.save_path, "w") as f:
+                    f.write(out)
 
 
 def render(page_list, config):
